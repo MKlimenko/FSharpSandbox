95bff6f8b1e1be1080dd66589ebdd121fa03dec9
m.klimenko <mklimenko29@gmail.com>
2019-03-30 23:20:39 +0300

    Cached Fibonacci

--- FSharpSandbox/Program.fs
+++ FSharpSandbox/Program.fs
@@ -2,7 +2,22 @@
 
 open System
 
+let Fib number =
+    let lookup = Array.create (number + 1) 0
+    let rec fib_internal = fun current_number ->
+        match current_number with
+        | 1 | 2 -> 1
+        | current_number ->
+            match lookup.[current_number] with
+            | 0 ->
+                lookup.[current_number] <- fib_internal(current_number-1) + fib_internal(current_number-2)
+                lookup.[current_number]
+            | x -> x
+    fib_internal(number)
+
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Hello World from F#! %d" (Fib 18)
     0 // return an integer exit code
